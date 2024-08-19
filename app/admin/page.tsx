c0ca04a9eaf3a70455ecc57368a54eb9e1f8064c
Mahtab <mahtaab.babaei@gmail.com>
2024-08-19 13:33:07 +0430

    Handle errors

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { Button, Flex, Grid, Text, TextField } from "@radix-ui/themes";
+import { Button, Callout, Flex, Grid, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
 
 interface PostForm {
   title: string;
@@ -13,39 +15,54 @@ interface PostForm {
 }
 
 const AdminPage = () => {
-  const router = useRouter()
+  const router = useRouter();
   const { register, control, handleSubmit } = useForm<PostForm>();
-  console.log(register("title"));
+  const [error, setError] = useState("");
 
   return (
-    <form
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/blog", data);
-        router.push('/blog')
-      })}
-    >
-      <Grid gap="5" className="max-w-xl">
-        <Grid gap="3">
-          <Text>عنوان</Text>
-          <TextField.Root placeholder="پست جدید" {...register("title")} />
+    <>
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/blog", data);
+            router.push("/blog");
+          } catch (error) {
+            setError("متاسفیم، یک خطای غیر منتظره رخ داد!");
+          }
+        })}
+      >
+        <Grid gap="5" className="max-w-xl">
+          <Text>ایجاد پست جدید</Text>
+          {error && (
+            <Callout.Root className="bg-opacity-20 bg-accent text-accent">
+              <Callout.Icon>
+                <InfoCircledIcon />
+              </Callout.Icon>
+              <Callout.Text>{error}</Callout.Text>
+            </Callout.Root>
+          )}
+          <Grid gap="3">
+            <Text>عنوان</Text>
+            <TextField.Root placeholder="پست جدید" {...register("title")} />
+          </Grid>
+          <Grid gap="3">
+            <Text>توضیحات</Text>
+            <Controller
+              name="description"
+              control={control}
+              render={({ field }) => <SimpleMDE {...field} />}
+            />
+          </Grid>
+          <Grid gap="3">
+            <Text>تصویر</Text>
+            <TextField.Root placeholder="آدرس تصویر" {...register("image")} />
+          </Grid>
+          <Flex justify="center">
+            <Button>ثبت پست جدید</Button>
+          </Flex>
         </Grid>
-        <Grid gap="3">
-          <Text>توضیحات</Text>
-          <Controller
-            name="description"
-            control={control}
-            render={({ field }) => <SimpleMDE {...field} />}
-          />
-        </Grid>
-        <Grid gap="3">
-          <Text>تصویر</Text>
-          <TextField.Root placeholder="آدرس تصویر" {...register("image")} />
-        </Grid>
-        <Flex justify="center">
-          <Button>ثبت پست جدید</Button>
-        </Flex>
-      </Grid>
-    </form>
+      </form>
+    </>
   );
 };
 
--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -3,8 +3,8 @@ import { z } from "zod";
 import prisma from "@/prisma/client";
 
 const createPostSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, 'Title is required.').max(255),
+  description: z.string().min(1, 'Description is required.'),
 });
 
 export async function POST(request: NextRequest) {
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest) {
   const validation = createPostSchema.safeParse(body);
 
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newPost = await prisma.post.create({
     data: { title: body.title, description: body.description },
