de8f055a9c3edc4f8efad77a80000e6f8e9164e1
Mahtab <mahtaab.babaei@gmail.com>
2024-08-19 11:06:10 +0430

    Handle form submission

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,33 +1,51 @@
 "use client";
-import {
-  Button,
-  Flex,
-  Grid,
-  Text,
-  TextField,
-} from "@radix-ui/themes";
+import { Button, Flex, Grid, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface PostForm {
+  title: string;
+  description: string;
+  image?: string;
+}
 
 const AdminPage = () => {
+  const router = useRouter()
+  const { register, control, handleSubmit } = useForm<PostForm>();
+  console.log(register("title"));
+
   return (
-    <Grid gap="5" className="max-w-xl">
-      <Grid gap="3">
-        <Text>عنوان</Text>
-        <TextField.Root placeholder="پست جدید" />
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/blog", data);
+        router.push('/blog')
+      })}
+    >
+      <Grid gap="5" className="max-w-xl">
+        <Grid gap="3">
+          <Text>عنوان</Text>
+          <TextField.Root placeholder="پست جدید" {...register("title")} />
+        </Grid>
+        <Grid gap="3">
+          <Text>توضیحات</Text>
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => <SimpleMDE {...field} />}
+          />
+        </Grid>
+        <Grid gap="3">
+          <Text>تصویر</Text>
+          <TextField.Root placeholder="آدرس تصویر" {...register("image")} />
+        </Grid>
+        <Flex justify="center">
+          <Button>ثبت پست جدید</Button>
+        </Flex>
       </Grid>
-      <Grid gap="3">
-        <Text>توضیحات</Text>
-        <SimpleMDE />
-      </Grid>
-      <Grid gap="3">
-        <Text>تصویر</Text>
-        <TextField.Root placeholder="آدرس تصویر" />
-      </Grid>
-      <Flex justify="center">
-        <Button>ثبت پست جدید</Button>
-      </Flex>
-    </Grid>
+    </form>
   );
 };
 
