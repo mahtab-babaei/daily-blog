a804aeec7967911664ff1e5f42df7f98f29bb269
Mahtab <mahtaab.babaei@gmail.com>
2024-08-31 10:52:35 +0430

    Handle errors

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Flex, Grid, Text, TextField } from "@radix-ui/themes";
+import { Callout, Flex, Grid, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import "./customEditor.css";
@@ -7,6 +7,8 @@ import DarkButton from "../components/DarkButton";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { InfoCircledIcon } from "@radix-ui/react-icons";
 
 interface PostForm {
   title: string;
@@ -17,19 +19,32 @@ interface PostForm {
 const AdminPage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<PostForm>();
+  const [error, setError] = useState("");
 
   return (
     <form
       className="flex justify-center"
       onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/blog", data);
-        router.push("/blog");
+        try {
+          await axios.post("/api/blog", data);
+          router.push("/blog");
+        } catch (error) {
+          setError("متاسفیم، یک خطای غیر منتظره رخ داد!");
+        }
       })}
     >
       <Grid gap="5" className="w-full max-w-md md:max-w-lg lg:max-w-2xl">
         <Text className="font-extrabold text-lg text-accent">
           <Text className="text-primary">ایجاد</Text> پست جدید
         </Text>
+        {error && (
+          <Callout.Root className="bg-opacity-5 bg-accent text-accent">
+            <Callout.Icon>
+              <InfoCircledIcon />
+            </Callout.Icon>
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        )}
         <Grid gap="3">
           <Text className="text-dark font-medium">عنوان</Text>
           <TextField.Root
