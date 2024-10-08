ea66681d6dec9e26afcf92975103b10715c0ed69
Mahtab <mahtaab.babaei@gmail.com>
2024-10-08 15:35:55 +0330

    Handle signin form submission and errors

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,45 +1,86 @@
-import { DarkButton } from "@/app/components";
+"use client";
+import { DarkButton, ErrorCallout } from "@/app/components";
+import { userSchema } from "@/app/validationSchemas";
 import { Flex, Grid, Text, TextField } from "@radix-ui/themes";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+type signinForm = z.infer<typeof userSchema>;
 
 const SignInPage = () => {
+  const { register, handleSubmit } = useForm<signinForm>();
+  const [error, setError] = useState("");
+  const router = useRouter();
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: data.email,
+        password: data.password,
+      });
+      if (result?.error) {
+        setSubmitting(false);
+        setError("ایمیل یا رمز عبور اشتباهه، دوباره امتحان کن.");
+      } else {
+        router.push("/admin");
+      }
+    } catch (error) {
+      setSubmitting(false);
+      setError("متاسفیم، یک خطای غیر منتظره رخ داد!");
+    }
+  });
+
   return (
     <Flex justify="center">
       <Grid gap="8" className="w-full max-w-md md:max-w-lg">
         <Text className="font-extrabold text-lg text-accent">
           <Text className="text-primary">ورود به</Text> حساب کاربری
         </Text>
-        <Grid gap="3">
-          <Text className="text-dark text-base font-medium">ایمیل</Text>
-          <TextField.Root
-            type="email"
-            size="3"
-            variant="soft"
-            className="bg-white p-2 text-sm"
-            placeholder="ایمیل خود را وارد کنید"
-          />
-        </Grid>
-        <Grid gap="3">
-          <Text className="text-dark text-base font-medium">رمز عبور</Text>
-          <TextField.Root
-            type="password"
-            size="3"
-            variant="soft"
-            className="bg-white p-2 text-sm"
-            placeholder="●●●●●●●"
-          />
-        </Grid>
-        <Flex justify="center">
+        <form onSubmit={onSubmit}>
           <Grid gap="5">
+            <ErrorCallout>{error}</ErrorCallout>
+            <Grid gap="3">
+              <Text className="text-dark text-base font-medium">ایمیل</Text>
+              <TextField.Root
+                type="email"
+                size="3"
+                variant="soft"
+                className="bg-white p-2 text-sm"
+                placeholder="ایمیل خود را وارد کنید"
+                {...register("email")}
+              />
+            </Grid>
+            <Grid gap="3">
+              <Text className="text-dark text-base font-medium">رمز عبور</Text>
+              <TextField.Root
+                type="password"
+                size="3"
+                variant="soft"
+                className="bg-white p-2 text-sm"
+                placeholder="●●●●●●●"
+                {...register("password")}
+              />
+            </Grid>
             <Flex justify="center">
-              <DarkButton>ورود</DarkButton>
+              <DarkButton isSubmitting={isSubmitting}>ورود</DarkButton>
             </Flex>
-            <Text className="text-primary">
-              حساب کاربری ندارید؟ <Link className="text-accent font-bold" href="/auth/signup">ثبت نام کنید</Link>
-            </Text>
+            <Flex justify="center">
+              <Text className="text-primary">
+                حساب کاربری ندارید؟{" "}
+                <Link className="text-accent font-bold" href="/auth/signup">
+                  ثبت نام کنید
+                </Link>
+              </Text>
+            </Flex>
           </Grid>
-        </Flex>
+        </form>
       </Grid>
     </Flex>
   );
