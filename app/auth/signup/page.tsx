57cc055ff36b9589f9a1d4f9a021f3d323239eca
Mahtab <mahtaab.babaei@gmail.com>
2024-10-08 10:00:40 +0330

    Handle signup form submission

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,71 +1,93 @@
+"use client";
 import { DarkButton } from "@/app/components";
+import { userSchema } from "@/app/validationSchemas";
 import { Flex, Grid, TextField, Text } from "@radix-ui/themes";
+import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+type signupForm = z.infer<typeof userSchema>;
 
 const SignUpPage = () => {
+  const { register, handleSubmit } = useForm<signupForm>();
+  const router = useRouter();
+
   return (
     <Flex justify="center">
-      <Grid gap="8" className="w-full max-w-md md:max-w-lg">
-        <Text className="font-extrabold text-lg text-primary">
-          <Text className="text-accent">ایجاد</Text> حساب کاربری
-        </Text>
-        <Grid gap="3">
-          <Text className="text-dark text-base font-medium">نام کاربری</Text>
-          <TextField.Root
-            type="text"
-            size="3"
-            variant="soft"
-            className="bg-white p-2 text-sm"
-            placeholder="انتخاب نام کاربری"
-          />
+      <form
+        className="w-full max-w-md md:max-w-lg"
+        onSubmit={handleSubmit(async (data) => {
+          await axios.post("/api/auth/signup", data);
+          router.push("/admin");
+        })}
+      >
+        <Grid gap="8">
+          <Text className="font-extrabold text-lg text-primary">
+            <Text className="text-accent">ایجاد</Text> حساب کاربری
+          </Text>
           <Grid gap="3">
-            <Text className="text-dark text-base font-medium">ایمیل</Text>
+            <Text className="text-dark text-base font-medium">نام کاربری</Text>
             <TextField.Root
-              type="email"
+              {...register("name")}
+              type="text"
               size="3"
               variant="soft"
               className="bg-white p-2 text-sm"
-              placeholder="ایمیل شما"
+              placeholder="انتخاب نام کاربری"
+            />
+            <Grid gap="3">
+              <Text className="text-dark text-base font-medium">ایمیل</Text>
+              <TextField.Root
+                {...register("email")}
+                type="email"
+                size="3"
+                variant="soft"
+                className="bg-white p-2 text-sm"
+                placeholder="ایمیل شما"
+              />
+            </Grid>
+          </Grid>
+          <Grid gap="3">
+            <Text className="text-dark text-base font-medium">رمز عبور</Text>
+            <TextField.Root
+              {...register("password")}
+              type="password"
+              size="3"
+              variant="soft"
+              className="bg-white p-2 text-sm"
+              placeholder="●●●●●●●"
             />
           </Grid>
-        </Grid>
-        <Grid gap="3">
-          <Text className="text-dark text-base font-medium">رمز عبور</Text>
-          <TextField.Root
-            type="password"
-            size="3"
-            variant="soft"
-            className="bg-white p-2 text-sm"
-            placeholder="●●●●●●●"
-          />
-        </Grid>
-        <Grid gap="3">
-          <Text className="text-dark text-base font-medium">
-            تکرار رمز عبور
-          </Text>
-          <TextField.Root
-            type="password"
-            size="3"
-            variant="soft"
-            className="bg-white p-2 text-sm"
-            placeholder="●●●●●●●"
-          />
-        </Grid>
-        <Flex justify="center">
-          <Grid gap="5">
-            <Flex justify="center">
-              <DarkButton>ثبت نام</DarkButton>
-            </Flex>
-            <Text className="text-primary">
-              حساب کاربری دارید؟{" "}
-              <Link className="text-accent font-bold" href="/auth/signin">
-                وارد شوید
-              </Link>
+          <Grid gap="3">
+            <Text className="text-dark text-base font-medium">
+              تکرار رمز عبور
             </Text>
+            <TextField.Root
+              {...register("confirmPassword")}
+              type="password"
+              size="3"
+              variant="soft"
+              className="bg-white p-2 text-sm"
+              placeholder="●●●●●●●"
+            />
           </Grid>
-        </Flex>
-      </Grid>
+          <Flex justify="center">
+            <Grid gap="5">
+              <Flex justify="center">
+                <DarkButton>ثبت نام</DarkButton>
+              </Flex>
+              <Text className="text-primary">
+                حساب کاربری دارید؟{" "}
+                <Link className="text-accent font-bold" href="/auth/signin">
+                  وارد شوید
+                </Link>
+              </Text>
+            </Grid>
+          </Flex>
+        </Grid>
+      </form>
     </Flex>
   );
 };
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -11,16 +11,24 @@ export const postSchema = z.object({
   image: z.string(),
 });
 
-export const userSchema = z.object({
-  name: z
-    .string()
-    .min(3, { message: "نام کاربری نباید کمتر از 3 کاراکتر باشد." })
-    .max(50, { message: "نام کاربری نباید بیشتر از 50 کاراکتر باشد." }),
-  email: z
-    .string()
-    .min(1, { message: "لطفا ایمیل خود را وارد کنید." })
-    .email({ message: "آدرس ایمیل وارد شده معتبر نیست." }),
-  password: z
-    .string()
-    .min(6, { message: "رمز عبور نباید کمتر از 6 کاراکتر باشد." }),
-});
+export const userSchema = z
+  .object({
+    name: z
+      .string()
+      .min(3, { message: "نام کاربری نباید کمتر از 3 کاراکتر باشد." })
+      .max(50, { message: "نام کاربری نباید بیشتر از 50 کاراکتر باشد." }),
+    email: z
+      .string()
+      .min(1, { message: "لطفا ایمیل خود را وارد کنید." })
+      .email({ message: "آدرس ایمیل وارد شده معتبر نیست." }),
+    password: z
+      .string()
+      .min(6, { message: "کلمه عبور نباید کمتر از 6 کاراکتر باشد." }),
+    confirmPassword: z
+      .string()
+      .min(6, "کلمه عبور نباید کمتر از 6 کاراکتر باشد."),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "کلمات عبور باهم یکسان نیستند.",
+    path: ["confirmPassword"],
+  });
