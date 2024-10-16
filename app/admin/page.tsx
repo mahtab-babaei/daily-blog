2dd34f0f2056eb8983b170c9a2e0d879cd8d2800
Mahtab <mahtaab.babaei@gmail.com>
2024-10-16 08:28:02 +0330

    Show the user name in admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,22 @@
 import { Flex, Grid, Text } from "@radix-ui/themes";
 import CreateNewPost from "./new/CreateNewPost";
 import PostsSummary from "./userPosts/PostsSummary";
+import { getServerSession } from "next-auth";
+import authOptions from "../auth/authOptions";
+import prisma from "@/prisma/client";
 
-const AdminPage = () => {
+const AdminPage = async () => {
+  const session = await getServerSession(authOptions);
+  const user = await prisma.user.findUnique({
+    where: { id: session!.user.id },
+  });
+
   return (
     <Flex justify="center">
       <Grid gap="8" className="w-full max-w-md md:max-w-lg lg:max-w-2xl">
+        <Text className="font-extrabold text-lg text-accent">
+          <Text className="text-primary">{user!.name} عزیـز،</Text> خوش اومدی!
+        </Text>{" "}
         <Text className="font-extrabold text-lg text-accent">
           <Text className="text-primary">ایجاد</Text> پست جدید
         </Text>
