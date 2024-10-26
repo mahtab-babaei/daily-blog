3dea0b8e3cb9dc41c01992db8aee2e567e5f8701
Mahtab <mahtaab.babaei@gmail.com>
2024-10-26 10:17:38 +0330

    Remove tooltip from activeUsers section

--- app/ActiveUsers.tsx
+++ app/ActiveUsers.tsx
@@ -1,6 +1,5 @@
 import prisma from "@/prisma/client";
 import noAvatar from "@/public/images/avatars/noAvatar.png";
-import * as Tooltip from "@radix-ui/react-tooltip";
 import { Flex, Text } from "@radix-ui/themes";
 import Image from "next/image";
 
@@ -10,9 +9,10 @@ const ActiveUsers = async () => {
     orderBy: { posts: { _count: "desc" } },
     take: 3,
   });
+  console.log("Active Users:", activeUsers);
 
   return (
-    <Tooltip.Provider>
+    <>
       <div className="custom:grid flex items-center justify-between gap-5 max-w-lg">
         <Text className="font-extrabold text-lg text-accent text-center">
           <Text className="text-primary">فعال ترین کـاربران </Text>
@@ -21,28 +21,19 @@ const ActiveUsers = async () => {
         <Flex align="center" justify="center">
           <Text className="font-extrabold text-lg text-light ml-2">2+</Text>
           {activeUsers.map((user) => (
-            <Tooltip.Root key={user.id}>
-              <Tooltip.Trigger asChild>
-                <Image
-                  className="rounded-full -ml-3"
-                  width="48"
-                  height="48"
-                  priority
-                  alt="activeUser"
-                  src={user.image || noAvatar}
-                />
-              </Tooltip.Trigger>
-              <Tooltip.Portal>
-                <Tooltip.Content className="bg-dark text-white p-2 rounded-lg text-xs">
-                  {user.name}
-                  <Tooltip.Arrow className="fill-dark" />
-                </Tooltip.Content>
-              </Tooltip.Portal>
-            </Tooltip.Root>
+            <Image
+              key={user.id}
+              className="rounded-full -ml-3"
+              width="48"
+              height="48"
+              priority
+              alt="activeUser"
+              src={user.image || noAvatar}
+            />
           ))}
         </Flex>
       </div>
-    </Tooltip.Provider>
+    </>
   );
 };
 
